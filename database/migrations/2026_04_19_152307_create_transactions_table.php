97d6ed6c6c99c99e391e4438f84140511c342055
Han Min Thaw <hanminthaw15@gmail.com>
2026-04-19 22:47:55 +0700

    Add migration files for categories, transactions, and budgets tables

---
+++ database/migrations/2026_04_19_152307_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['income', 'expense']);
+            $table->decimal('amount', 15,2);
+            $table->date('date');
+            $table->text('description')->nullable();
+            $table->string('account')->nullable();
+            $table->string('reference')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            $table->index(['user_id', 'date', 'type']);
+            $table->index('category_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
